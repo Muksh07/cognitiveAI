319845384480acf913b9702d8b11006e3f9ce4e1
Muksh07 <166619242+Muksh07@users.noreply.github.com>
2024-07-07 19:33:00 +0530

    folder structure and file content

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ce7e1eaddcb6a1298d8a5d5c32600364e215592")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2f48277d296144ee208c7a35c06e5ed21d927f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
